a92b167fa1e3ac1dbd440a5331808e76ad3405d9
kkomorii <155421184+kkomorii@users.noreply.github.com>
2024-08-01 14:34:24 +0900

    Update CalenderActivity.kt
    일정이 비어있을 시 입력칸만 나오도록 수정

--- app/src/main/java/com/guru2/todolist/CalenderActivity.kt
+++ app/src/main/java/com/guru2/todolist/CalenderActivity.kt
@@ -1,8 +1,8 @@
-package com.guru2.todolist
+package com.example.calender
+
 import android.annotation.SuppressLint
 import java.io.FileInputStream
 import java.io.FileOutputStream
-
 import android.view.View
 import android.os.Bundle
 import android.widget.Button
@@ -11,33 +11,32 @@ import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
-class CalenderActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     var userID: String = "userID"
     lateinit var fname: String
     lateinit var str: String
     lateinit var calendarView: CalendarView
     lateinit var updateBtn: Button
-    lateinit var deleteBtn:Button
-    lateinit var saveBtn:Button
+    lateinit var deleteBtn: Button
+    lateinit var saveBtn: Button
     lateinit var diaryTextView: TextView
-    lateinit var diaryContent:TextView
-    lateinit var title:TextView
+    lateinit var diaryContent: TextView
+    lateinit var title: TextView
     lateinit var contextEditText: EditText
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_calender)
+        setContentView(R.layout.activity_main)
 
         // UI값 생성
-        calendarView=findViewById(R.id.calendarView)
-        diaryTextView=findViewById(R.id.diaryTextView)
-        saveBtn=findViewById(R.id.saveBtn)
-        deleteBtn=findViewById(R.id.deleteBtn)
-        updateBtn=findViewById(R.id.updateBtn)
-        diaryContent=findViewById(R.id.diaryContent)
-        title=findViewById(R.id.title)
-        contextEditText=findViewById(R.id.contextEditText)
+        calendarView = findViewById(R.id.calendarView)
+        diaryTextView = findViewById(R.id.diaryTextView)
+        saveBtn = findViewById(R.id.saveBtn)
+        deleteBtn = findViewById(R.id.deleteBtn)
+        updateBtn = findViewById(R.id.updateBtn)
+        diaryContent = findViewById(R.id.diaryContent)
+        title = findViewById(R.id.title)
+        contextEditText = findViewById(R.id.contextEditText)
 
         title.text = "SWUCHEDULE"
 
@@ -67,8 +66,8 @@ class CalenderActivity : AppCompatActivity() {
 
     // 달력 내용 조회, 수정
     fun checkDay(cYear: Int, cMonth: Int, cDay: Int, userID: String) {
-        //저장할 파일 이름설정
-        fname = "" + userID + cYear + "-" + (cMonth + 1) + "" + "-" + cDay + ".txt"
+        // 저장할 파일 이름설정
+        fname = "$userID$cYear-${cMonth + 1}-$cDay.txt"
 
         var fileInputStream: FileInputStream
         try {
@@ -77,69 +76,70 @@ class CalenderActivity : AppCompatActivity() {
             fileInputStream.read(fileData)
             fileInputStream.close()
             str = String(fileData)
-            contextEditText.visibility = View.INVISIBLE
-            diaryContent.visibility = View.VISIBLE
-            diaryContent.text = str
-            saveBtn.visibility = View.INVISIBLE
-            updateBtn.visibility = View.VISIBLE
-            deleteBtn.visibility = View.VISIBLE
-            updateBtn.setOnClickListener {
+
+            if (str.isEmpty()) {
                 contextEditText.visibility = View.VISIBLE
-                diaryContent.visibility = View.INVISIBLE
-                contextEditText.setText(str)
                 saveBtn.visibility = View.VISIBLE
-                updateBtn.visibility = View.INVISIBLE
-                deleteBtn.visibility = View.INVISIBLE
-                diaryContent.text = contextEditText.text
-            }
-            deleteBtn.setOnClickListener {
                 diaryContent.visibility = View.INVISIBLE
                 updateBtn.visibility = View.INVISIBLE
                 deleteBtn.visibility = View.INVISIBLE
-                contextEditText.setText("")
-                contextEditText.visibility = View.VISIBLE
-                saveBtn.visibility = View.VISIBLE
-                removeDiary(fname)
+            } else {
+                contextEditText.visibility = View.INVISIBLE
+                diaryContent.visibility = View.VISIBLE
+                diaryContent.text = str
+                saveBtn.visibility = View.INVISIBLE
+                updateBtn.visibility = View.VISIBLE
+                deleteBtn.visibility = View.VISIBLE
+
+                updateBtn.setOnClickListener {
+                    contextEditText.visibility = View.VISIBLE
+                    diaryContent.visibility = View.INVISIBLE
+                    contextEditText.setText(str)
+                    saveBtn.visibility = View.VISIBLE
+                    updateBtn.visibility = View.INVISIBLE
+                    deleteBtn.visibility = View.INVISIBLE
+                    diaryContent.text = contextEditText.text
+                }
+
+                deleteBtn.setOnClickListener {
+                    diaryContent.visibility = View.INVISIBLE
+                    updateBtn.visibility = View.INVISIBLE
+                    deleteBtn.visibility = View.INVISIBLE
+                    contextEditText.setText("")
+                    contextEditText.visibility = View.VISIBLE
+                    saveBtn.visibility = View.VISIBLE
+                    removeDiary(fname)
+                }
             }
-            if (diaryContent.text == null) {
-                diaryContent.visibility = View.INVISIBLE
-                updateBtn.visibility = View.INVISIBLE
-                deleteBtn.visibility = View.INVISIBLE
-                diaryTextView.visibility = View.VISIBLE
-                saveBtn.visibility = View.VISIBLE
-                contextEditText.visibility = View.VISIBLE
-            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            contextEditText.visibility = View.VISIBLE
+            saveBtn.visibility = View.VISIBLE
+            diaryContent.visibility = View.INVISIBLE
+            updateBtn.visibility = View.INVISIBLE
+            deleteBtn.visibility = View.INVISIBLE
+        }
+    }
+
+    // 달력 내용 제거
+    @SuppressLint("WrongConstant")
+    fun removeDiary(readDay: String?) {
+        try {
+            deleteFile(readDay)
         } catch (e: Exception) {
             e.printStackTrace()
         }
     }
 
-
-    // 달력 내용 제거
-    @SuppressLint("WrongConstant")
-    fun removeDiary(readDay: String?) {
-        var fileOutputStream: FileOutputStream
-        try {
-            fileOutputStream = openFileOutput(readDay, MODE_NO_LOCALIZED_COLLATORS)
-            val content = ""
-            fileOutputStream.write(content.toByteArray())
-            fileOutputStream.close()
-        } catch (e: java.lang.Exception) {
-            e.printStackTrace()
-        }
-    }
-
-
     // 달력 내용 추가
     @SuppressLint("WrongConstant")
     fun saveDiary(readDay: String?) {
-        var fileOutputStream: FileOutputStream
         try {
-            fileOutputStream = openFileOutput(readDay, MODE_NO_LOCALIZED_COLLATORS)
+            val fileOutputStream = openFileOutput(readDay, MODE_NO_LOCALIZED_COLLATORS)
             val content = contextEditText.text.toString()
             fileOutputStream.write(content.toByteArray())
             fileOutputStream.close()
-        } catch (e: java.lang.Exception) {
+        } catch (e: Exception) {
             e.printStackTrace()
         }
     }

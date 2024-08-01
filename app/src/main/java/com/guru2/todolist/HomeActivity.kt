1efc5bcce4f387551abbe11480c8522ca59aab78
tethys0310 <tethys0310@gmail.com>
2024-08-01 13:16:34 +0900

    투두리스트 이동창 메인액티비티에서 홈액티비티로 이동

--- app/src/main/java/com/guru2/todolist/HomeActivity.kt
+++ app/src/main/java/com/guru2/todolist/HomeActivity.kt
@@ -1,6 +1,8 @@
 package com.guru2.todolist
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,17 +10,23 @@ import androidx.core.view.WindowInsetsCompat
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        val buttonTodo : Button = findViewById(R.id.button_todo)
+
+
+        val buttonTodo: Button = findViewById(R.id.button_todo)
+
         buttonTodo.setOnClickListener {
-          val intent = Intent(this, TodoActivityTest::class.java)
-          startActivity(intent)
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
+            val intent = Intent(this, TodoActivityTest::class.java)
+            startActivity(intent)
+
+            super.onCreate(savedInstanceState)
+            enableEdgeToEdge()
+            setContentView(R.layout.activity_home)
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.home)) { v, insets ->
 //            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
 //            insets
 //        }
+        }
     }
+}
    

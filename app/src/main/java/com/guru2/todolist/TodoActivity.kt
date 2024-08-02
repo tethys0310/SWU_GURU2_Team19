e698d3d65a45cb8e431b21c8caf7ffefe41f99b0
oOHAMSOo <153520967+oOHAMSOo@users.noreply.github.com>
2024-08-02 16:39:59 +0900

    네비게이션 오류 해결

--- app/src/main/java/com/guru2/todolist/TodoActivity.kt
+++ app/src/main/java/com/guru2/todolist/TodoActivity.kt
@@ -9,6 +9,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ListView
 import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import io.github.jan.supabase.postgrest.Postgrest
 import io.github.jan.supabase.postgrest.postgrest
@@ -160,6 +161,7 @@ class TodoActivity : MenuTestActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_todo_test)
         setupBottomNavigationBar(R.id.nav_todo)
 

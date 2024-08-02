8e45f00033adeb314faf3bd660119b201567c40d
oOHAMSOo <153520967+oOHAMSOo@users.noreply.github.com>
2024-08-02 15:19:07 +0900

    홈 네비게이션 수정

--- app/src/main/java/com/guru2/todolist/HomeActivity.kt
+++ app/src/main/java/com/guru2/todolist/HomeActivity.kt
@@ -8,23 +8,22 @@ import androidx.activity.enableEdgeToEdge
 class HomeActivity : MenuTestActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
-
-        val buttonTodo: Button = findViewById(R.id.button_todo)
-
-        buttonTodo.setOnClickListener {
-            val intent = Intent(this, TodoActivity::class.java)
-            startActivity(intent)
-
-            super.onCreate(savedInstanceState)
-            enableEdgeToEdge()
-            setContentView(R.layout.activity_home)
-            setupBottomNavigationBar(R.id.nav_todo)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.home)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home)
+        setupBottomNavigationBar(R.id.nav_todo)
+//        val buttonTodo: Button = findViewById(R.id.button_todo)
+//
+//        buttonTodo.setOnClickListener {
+//            val intent = Intent(this, TodoActivity::class.java)
+//            startActivity(intent)
+//
+////        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.home)) { v, insets ->
+////            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+////            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+////            insets
+////        }
 //        }
-        }
     }
 }
    
